affe8802f565f1417e0c8b86db8bc09b9012669b
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-30 22:49:31 +0530

    routing

---
+++ src/app/navbar-employee/navbar-employee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-employee',
+  templateUrl: './navbar-employee.component.html',
+  styleUrls: ['./navbar-employee.component.css']
+})
+export class NavbarEmployeeComponent {
+
+}
