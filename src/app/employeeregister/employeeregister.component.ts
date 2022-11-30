be92d06b9caa183c58605aa5871e5d6cd74b6fa4
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-30 16:30:37 +0530

    employeeregister

---
+++ src/app/employeeregister/employeeregister.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employeeregister',
+  templateUrl: './employeeregister.component.html',
+  styleUrls: ['./employeeregister.component.css']
+})
+export class EmployeeregisterComponent {
+
+}
